67e7362e99d6424316f60b745c9734182b7139bf
DenysVM <muratov.d@gmail.com>
2024-02-04 12:14:11 +0100

    10.1 Routes fix

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/goit-test"> {/* Укажите basename здесь */}
+        <BrowserRouter basename="/goit-test">
           <App />
         </BrowserRouter>
       </PersistGate>
