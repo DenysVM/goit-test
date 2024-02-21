90614a60f28e0aa6162ee1f0a21fe60e44d60d14
DenysVM <muratov.d@gmail.com>
2024-02-21 13:02:20 +0100

    06. Додавання burger menu та відповідна стилізація для мобільної версії застосунка

--- src/components/common/NavigationMenu.js
+++ src/components/common/NavigationMenu.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import useMediaQuery from '../../hooks/useMediaQuery';
 import '../../styles/NavStyles.css';
 
 function Navigation() {
+    const isDesktop = useMediaQuery('(min-width: 769px)');
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleCloseMenu = () => {
+        if (!isDesktop) {
+            setMenuOpen(false);
+        }
+    };
+
     return (
         <nav className="navbar">
-            <NavLink to="/" activeclassname="active">Головна</NavLink>
-            <ul>
-                <li>
-                    <NavLink to="/catalog" activeclassname="active">Каталог</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/favorites" activeclassname="active">Обране</NavLink>
-                </li>
-            </ul>
+            {!isDesktop && (
+                <button className="menu-button" aria-label="Toggle menu" onClick={() => setMenuOpen(!menuOpen)}>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </button>
+            )}
+            <div className={`menu-content ${!isDesktop && menuOpen ? 'show' : 'hide'}`}>
+                <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')} onClick={handleCloseMenu}>Головна</NavLink>
+                <ul>
+                    <li onClick={handleCloseMenu}>
+                        <NavLink to="/catalog" className={({ isActive }) => (isActive ? 'active' : '')}>Каталог</NavLink>
+                    </li>
+                    <li onClick={handleCloseMenu}>
+                        <NavLink to="/favorites" className={({ isActive }) => (isActive ? 'active' : '')}>Обране</NavLink>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 }
