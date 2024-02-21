1697bf927bc7efb9b7143fc249a9559e9085b3df
DenysVM <muratov.d@gmail.com>
2024-02-21 13:15:57 +0100

    6.1 Анімація іконки бургер меню. Закриття бургер меню по трьох подіях: - натискання на хрестик - обрання пункту меню - натискання на екран за межами меню

--- src/components/common/NavigationMenu.js
+++ src/components/common/NavigationMenu.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import useMediaQuery from '../../hooks/useMediaQuery';
 import '../../styles/NavStyles.css';
@@ -6,23 +6,38 @@ import '../../styles/NavStyles.css';
 function Navigation() {
     const isDesktop = useMediaQuery('(min-width: 769px)');
     const [menuOpen, setMenuOpen] = useState(false);
+    const menuRef = useRef();
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                setMenuOpen(false);
+            }
+        };
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [menuRef]);
+
+    const handleToggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
     const handleCloseMenu = () => {
-        if (!isDesktop) {
-            setMenuOpen(false);
-        }
+        setMenuOpen(false);
     };
 
     return (
         <nav className="navbar">
             {!isDesktop && (
-                <button className="menu-button" aria-label="Toggle menu" onClick={() => setMenuOpen(!menuOpen)}>
-                    <span className="bar"></span>
-                    <span className="bar"></span>
-                    <span className="bar"></span>
+                <button className="menu-button" aria-label="Toggle menu" onClick={handleToggleMenu}>
+                    <span className={`bar ${menuOpen ? 'open' : ''}`}></span>
+                    <span className={`bar ${menuOpen ? 'open' : ''}`}></span>
+                    <span className={`bar ${menuOpen ? 'open' : ''}`}></span>
                 </button>
             )}
-            <div className={`menu-content ${!isDesktop && menuOpen ? 'show' : 'hide'}`}>
+            <div className={`menu-content ${!isDesktop && menuOpen ? 'show' : 'hide'}`} ref={menuRef}>
                 <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')} onClick={handleCloseMenu}>Головна</NavLink>
                 <ul>
                     <li onClick={handleCloseMenu}>
